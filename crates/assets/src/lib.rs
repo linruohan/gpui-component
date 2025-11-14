1425fdd10c6b5df20727c746f5d0134a03846ce4
linruohan <mjt1220@126.com>
2025-11-14 20:59:24 +0800

    update icon

--- crates/assets/src/lib.rs
+++ crates/assets/src/lib.rs
@@ -1,5 +1,5 @@
 use anyhow::anyhow;
-use gpui::{AssetSource, Result, SharedString};
+use gpui::{App, AssetSource, Result, SharedString};
 use rust_embed::RustEmbed;
 use std::borrow::Cow;
 
@@ -16,6 +16,9 @@ use std::borrow::Cow;
 #[derive(RustEmbed)]
 #[folder = "assets"]
 #[include = "icons/**/*.svg"]
+#[include = "planify-icons/**/*.svg"]
+#[include = "fonts/**/*.ttf"]
+#[exclude = "*.DS_Store"]
 pub struct Assets;
 
 impl AssetSource for Assets {
@@ -33,5 +36,33 @@ impl AssetSource for Assets {
         Ok(Self::iter()
             .filter_map(|p| p.starts_with(path).then(|| p.into()))
             .collect())
+    }
+}
+
+impl Assets {
+    /// Populate the [`TextSystem`] of the given [`AppContext`] with all `.ttf` fonts in the `fonts` directory.
+    pub fn load_fonts(&self, cx: &App) -> gpui::Result<()> {
+        let font_paths = self.list("fonts")?;
+        let mut embedded_fonts = Vec::new();
+        for font_path in font_paths {
+            if font_path.ends_with(".ttf") {
+                let font_bytes = cx
+                    .asset_source()
+                    .load(&font_path)?
+                    .expect("Assets should never return None");
+                embedded_fonts.push(font_bytes);
+            }
+        }
+
+        cx.text_system().add_fonts(embedded_fonts)
+    }
+
+    pub fn load_test_fonts(&self, cx: &App) {
+        cx.text_system()
+            .add_fonts(vec![self
+                .load("fonts/LXGWWenKaiGB-Regular.ttf")
+                .unwrap()
+                .unwrap()])
+            .unwrap()
     }
 }
