146885012ce7ab3c04c3d4c5f27061e5828f5981
linruohan <mjt1220@126.com>
2026-02-23 09:46:52 +0800

    add

--- update.sh
+++ update.sh
@@ -1,1 +1,4 @@
-git fetch long ;git checkout main;git merge long/main
+#  git remote add long ssh://git@ssh.github.com:443/longbridge/gpui-component
+git fetch long
+git checkout main
+git merge long/main
